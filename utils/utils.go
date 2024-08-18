058b31568a126b8fc999e1188300646a62f5b35f
mateochauchet <chauchetmateo@gmail.com>
2024-08-18 12:08:58 -0300

    add utils to write, parse, write error JSON

---
+++ utils/utils.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ParseJson(r *http.Request, payloadVar any) error {
+	if r.Body == nil {
+		return fmt.Errorf("empty body")
+	}
+
+	return json.NewDecoder(r.Body).Decode(&payloadVar)
+}
+
+func WriteJson(w http.ResponseWriter, status int, output any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(&output)
+}
+
+func WriteError(w http.ResponseWriter, status int, err error) error {
+	return WriteJson(w, status, map[string]string{"error": err.Error()})
+}
